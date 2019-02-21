60e30fece5da251a43785bbb36759f1b898072f2
Mario Garcia Valdez <mariosky@gmail.com>
2019-02-20 18:08:37 -0800

    ultimo cambios

--- subject_01.py
+++ subject_01.py
@@ -1,22 +1,15 @@
 from rx import Observable, Observer
 from rx.subjects import Subject
 
-
-
 AA = Subject()
-
 
 aa = AA.filter(lambda s: s == 'A') \
     .map(lambda s: s.lower()).publish()
 
-
 aa.subscribe(lambda a: print(a) )
-
 AA.subscribe(lambda a: print("{}".format(2)))
 
 aa.connect()
-
-
 Observable.from_("A,B,C,D,D,E,F,G,H".split(','))\
     .subscribe(AA.on_next)
 
