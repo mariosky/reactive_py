abe7a74c3f06085b7db7db9843c9f45a752ef1a8
mariosky <mariosky@gmail.com>
2019-10-02 11:42:24 -0700

    RxPy3

--- 05_Leer_iris.py
+++ 05_Leer_iris.py
@@ -1,14 +1,14 @@
 
 # Reactive
-from rx import Observable
+from rx import from_iterable , operators as op
 
 
 def read_iris(ruta='iris.data'):
     archivo = open(ruta)
-    return Observable.from_(archivo) \
-            .map(lambda linea : linea.split(',')[:-1] ) \
-            .filter(lambda linea : len(linea) > 3 ) \
-            .map(lambda linea : map(float,linea))
+    return from_iterable(archivo).pipe(
+           op.map(lambda linea : linea.split(',')[:-1] ), 
+           op.filter(lambda linea : len(linea) > 3 ), 
+           op.map(lambda linea : map(float,linea)))
 
 
 read_iris().subscribe(lambda linea : print(list(linea)))
